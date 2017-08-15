c33ad61c4a0ff1831e229b8d0bf53b617b723f22
alxmpotter <alxmpotter@gmail.com>
2017-08-15 10:52:27 -0500

    external and inline javascript
    added two html files for javascript

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("Hello from external JavaScript!");
+
+alert("Welcome to my Website.");
+
+var favColor =  prompt("What is your favorite color?");
+
+alert("Great, " + favColor + " is my favorite color too!");
