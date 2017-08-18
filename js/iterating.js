22399629d7b4ea33dd92b93a916693780c39e591
alxmpotter <alxmpotter@gmail.com>
2017-08-18 16:53:22 -0500

    for loop and forEach with arrays

--- js/iterating.js
+++ js/iterating.js
@@ -3,24 +3,28 @@
 
     var names = ["Luke", "Han-Solo", "Chewbaca", "Leia"];
 
-    console.log("Inside this array there are " + names.length + " names.");
+    //first iterating exercise
+    //
+    // console.log("Inside this array there are " + names.length + " names.");
+    //
+    // console.log("The first name is, " + names[0] + ".");
+    // console.log("The first name is, " + names[1] + ".");
+    // console.log("The first name is, " + names[2] + ".");
+    // console.log("The first name is, " + names[3] + ".");
+    //
+    // //different way to create array
+    // // var names = new Array("name1", "name2", "name3 ", "name4");
+    //
+    // //for loop
+    //
+    // for (var i = 0; i < names.length; i++) {
+    //     console.log(names[i] = names[i]);
+    //     // console.log("the name at index " + i + " is " + names[i]);
+    // }
 
-    console.log("The first name is, " + names[0] + ".");
-    console.log("The first name is, " + names[1] + ".");
-    console.log("The first name is, " + names[2] + ".");
-    console.log("The first name is, " + names[3] + ".");
+    //for each loop
 
-    //different way to create array
-    // var names = new Array("name1", "name2", "name3 ", "name4");
-
-    //for loop
-
-    for (var i = 0; i < names.length; i++) {
-        console.log(names[i] = names[i]);
-        // console.log("the name at index " + i + " is " + names[i]);
-    }
-
-     names.forEach( function (element, index, array) {
+     names.forEach( function (element) {
          console.log(element);
      });
 
