6eb920b332a91417d36c363730127ba705e3c32f
alxmpotter <alxmpotter@gmail.com>
2017-08-18 11:32:08 -0500

    finished iterating and planets exercises

--- js/iterating.js
+++ js/iterating.js
@@ -2,19 +2,14 @@
     "use strict";
     var names = ["Luke", "Han-Solo", "Chewbaca", "Leia"];
 
-    console.log(names.length);
+    console.log("Inside this array there are " + names.length + " names.");
+
+    console.log("The first name is, " + names[0] + ".");
+    console.log("The first name is, " + names[1] + ".");
+    console.log("The first name is, " + names[2] + ".");
+    console.log("The first name is, " + names[3] + ".");
 
 
-
-
-
-
-
-    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-
-    // TODO: Create a log statement that will log the number of elements in the names array.
-
-    // TODO: Create log statements that will print each of the names array elements individually.
 })();
 
 
