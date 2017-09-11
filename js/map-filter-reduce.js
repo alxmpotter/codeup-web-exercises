f502494b4744624a9885c7aea4af5d8ba64fa3a1
alxmpotter <alxmpotter@gmail.com>
2017-09-11 15:21:18 -0500

    map filter and reduce exercise
    *use filter to pick out users that know three or more computer languages
    *use map to create an array of strings that holds each users email

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,44 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+console.log(users);
+
+//return users that know 3 or more computer languages---------
+const moreThanThreeLanguages = users.filter(user => user.languages.length >= 3);
+console.log(moreThanThreeLanguages);
+
+//use map to create an array of strings that is ea users email--------
+let userEmail = users.map(user => user.email);
+console.log(userEmail);
+
+//reduce. transform array to object--------
