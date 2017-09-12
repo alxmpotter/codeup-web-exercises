b62701dd8e7687f4bdf213afe4f92c04aa1b8d5a
alxmpotter <alxmpotter@gmail.com>
2017-09-12 15:53:12 -0500

    Promises exercise
    *made small edits
    *delete comments no longer needed

--- js/promises.js
+++ js/promises.js
@@ -1,18 +1,21 @@
 "use strict";
 //make a function called wait, return a promise after x seconds
-// const wait = new Promise((resolve, reject) =>) {
-//     setTimeout(() => {
+const wait =  (milliseconds) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, milliseconds);
+    });
+};
+
+// function wait (number) {
+//     return new Promise( (resolve) => {
+//         setTimeout(() => resolve(), number);
 //
-//     }, 1800);
+//     });
+//
 // }
 
-function wait (number) {
-    return new Promise( (resolve) => {
-        setTimeout(() => resolve(), number);
-
-    });
-
-}
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
@@ -28,23 +31,18 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 // userCommit("alxmpotter");
 
-function lastCommit(owner, repo) {
-return fetch(`https://api.github.com/repos/${owner}/${repo}/events`,{headers: {"Authorization" : "token 02e7b36f39c8783bbaae6e025428cb42dff021df"}})
-    .then(function(response){
+function lastCommit(user, repo) {
+return fetch(`https://api.github.com/repos/${user}/${repo}/events`,
+    { headers:
+        {"Authorization" : "token 02e7b36f39c8783bbaae6e025428cb42dff021df"}
+    })
+    .then((response) => {
         return response.json();
     })
-    .then(function(event){
+    .then( (event) => {
         return event[0].created_at;
     });
 }
 
-lastCommit("alxmpotter", "codeup-web-exercises").then(response => console.log(response));
+lastCommit("alxmpotter", "codeup-web-exercises").then(event => console.log(event));
 
-// fetch('https://api.github.com/users').then((response) => {
-//     const users = response.json().then((users) => {
-//         const usernames = users.map(user => user.login);
-//         usernames.forEach((username) => {
-//            console.log(username);
-//         });
-//     });
-// });
